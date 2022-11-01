b32b67c7045450b1056593d24a2e1de887de99d6
Brett <comyune@gmail.com>
2022-11-01 14:29:37 +0000

    Add FizzBuzz edge cases

--- fizzbuzz_server/lib/fizzbuzz_server/generator.ex
+++ fizzbuzz_server/lib/fizzbuzz_server/generator.ex
@@ -6,6 +6,7 @@ defmodule FizzBuzzServer.Generator do
   @doc """
   Generates the FizzBuzz result for an integer.
   """
+  def call(input) when not is_integer(input), do: {:error, :input_must_be_an_integer}
   def call(input) when rem(input, 3) == 0 and rem(input, 5) == 0, do: "FizzBuzz"
   def call(input) when rem(input, 5) == 0, do: "Buzz"
   def call(input) when rem(input, 3) == 0, do: "Fizz"
